c0dafa75828bccd9fff673544fd49473840f8106
Your GitHub Username <your_email@example.com>
2024-11-21 20:55:34 +0000

    began the script

---
+++ script.R
@@ -0,0 +1,8 @@
+# Creating interactive visualisations: plotly, leaflet, DT, flextable and gganimate
+## Created by Emma House 
+### 21/11/2024
+
+# Set your working directory, this will be different for you depending on your filepath to the downloaded repository 
+setwd("~/Downloads/tutorial-emmmahouse")
+
+# Libraries
